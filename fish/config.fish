1766454e60d40f18cc92f8c8373d8524438b31af
wchen <wchen137@qq.com>
2024-05-21 17:17:07 +0800

    init: init config

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting ''
+    set PATH $PATH:/home/wchen/dev/go/bin
+    alias ll 'ls -la --color=auto'
+    alias gg 'lazygit'
+end
