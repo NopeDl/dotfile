d5d73a5632ec3a8d90b154aefa37b0eaf80129de
wchen <wchen137@qq.com>
2024-05-23 17:20:38 +0800

    fix: fix wildcard error when no files in directory

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set DEV_PATH $HOME/dev
 
-set PATH $PATH:/home/wchen/dev/go/bin
+# go
+set PATH $PATH:$DEV_PATH/go/bin
+
+# java
+set PATH $PATH:$DEV_PATH/jdks/jdk22/bin
+set JAVA_HOME $DEV_PATH/jdks/jdk22
+
+alias et=empty-trash
--- fish/functions/empty-trash.fish
+++ fish/functions/empty-trash.fish
@@ -1,7 +1,7 @@
 # 创建一个函数来清空回收站
 function empty-trash
   # 检查回收站目录是否存在
-  if test -d ~/deleted
+  if test -d ~/deleted; and test (count (ls ~/deleted)) -gt 0
     # 删除回收站目录及其内容
     command rm -rf ~/deleted/*
     echo "Trash is now empty."
